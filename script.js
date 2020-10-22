ceaeeaeaa69e2e4e232b3dbe2040d8ddb9e5e36c
abhisheksaini <saini.abhishek@hotmail.com>
2020-10-23 00:09:37 +0200

    click listener.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const _message = document.querySelector('.message');
+const _guess = document.querySelector('.guess');
+const _check = document.querySelector('.check');
+
+_check.addEventListener('click', function() {
+    const _guessValue = +_guess.value;
+    if (!_guessValue) {
+        _message.textContent = '⛔ No number!';
+    }
+});
